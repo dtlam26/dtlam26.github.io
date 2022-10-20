53a854aaff21b2c0a051b45eb26c42efd6b826fe
dtlam26 <adamduong26111996@gmail.com>
2022-10-20 22:29:57 +0900

    Projects Bug Fixed

---
+++ assets/js/reveal.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll('.tabs li')
+const tabs_content = document.querySelectorAll('#tabs_content > div')
+
+tabs.forEach((tab)=>{
+  tab.addEventListener('click',() => {
+    tabs.forEach(item=>item.classList.remove('is-active'))
+    tab.classList.add('is-active');
+
+    const target = tab.dataset.target
+    tabs_content.forEach(content =>{
+      if (content.getAttribute('id')==target){
+        content.classList.remove('is-hidden');
+      }
+      else{
+        content.classList.add('is-hidden');
+      }
+    })
+  })
+})
